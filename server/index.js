cb27a5e2a366a7e72a756af3cc2496bb6acfbf49
Negar Amiri <negamiri@gmail.com>
2018-12-13 23:53:56 +0000

    Adds mongo database where all tweets will be stored

--- server/index.js
+++ server/index.js
@@ -6,34 +6,32 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const DataHelpersFactory = require("./lib/data-helpers.js");
+const tweetsRoutes = require("./routes/tweets");
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// Database on Mongo
+const MongoClient   = require('mongodb').MongoClient;
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+let DataHelpers;
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+      console.log(`Failed to connect: ${MONGODB_URI}`);
+      throw err;
+  }
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-// //Post Tweets
-// app.post("/tweets/", (req, res) => {
+  DataHelpers = DataHelpersFactory(db);
 
-// });
+  app.use("/tweets", tweetsRoutes(DataHelpers));
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,17 +7,17 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection('tweets').find().toArray((err, tweets) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, tweets);
       });
     }
 
