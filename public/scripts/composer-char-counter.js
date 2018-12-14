2a9443aee59ddc082d70784dfcec4e3dd453a270
Negar Amiri <negamiri@gmail.com>
2018-12-14 02:50:20 +0000

    Removes unused variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
-    // --- our code goes here ---
+
     const maxLength = 140;
-    $(".tweettext").on('input', function(event) {
+    $(".tweettext").on('input', function() {
         const textLength = $(this).val().length;
         const textRemaining = maxLength - textLength;
         const counter = $(this).siblings(".counter");
